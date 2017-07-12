fb52323e75ca20883c90a38c512e932f1d0f0674
youngcy <youngcy.youngcy@gmail.com>
2017-07-12 19:49:33 +0800

    add more two distance function for utitls.hpp

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -5,7 +5,7 @@
 
 class Distance {
 public:
-    /*
+    /* Euclidean Distance
      * d = ((x1-x2)^2 + (y1-y2)^2 + ...)^(1/2)
      * */
     static double euclideanDistance(const Eigen::VectorXd &a,
@@ -17,6 +17,35 @@ public:
         }
 
         return c.sum();
+    }
+
+    /* Manhattan Distance
+     * d = |x1-x2| + |y1-y2| + ...
+     * */
+    static double manhattanDistance(const Eigen::VectorXd &a,
+                const Eigen::VectorXd &b) {
+        Eigen::VectorXd x = a - b;
+        Eigen::VectorXd c;
+
+        for (int i = 0; i < x.size(); ++i) {
+            c << abs(x[i]);
+        }
+
+        return c.sum();
+    }
+
+    /* Chebyshev Distance
+     * d = max(|x1-x2|, |y1-y2|, ...)
+     * */
+    static double chebyshevDistance(const Eigen::VectorXd &a,
+                const Eigen::VecotrXd &b) {
+        Eigen::VectorXd x = a - b;
+        Eigen::VectorXd c;
+
+        for (int i = 0; i < x.size(); ++i) {
+            c << abs(x[i]);
+        }
+        return (double)x.maxCoeff();
     }
 
 };
