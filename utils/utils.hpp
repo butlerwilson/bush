456f79a526644c568defdb674199e4c562102bfe
youngcy <youngcy.youngcy@gmail.com>
2017-07-12 19:25:52 +0800

    add utils.hpp

---
+++ utils/utils.hpp
@@ -0,0 +1,24 @@
+#ifndef __BUSH_UTILS_HPP__
+#define __BUSH_UTILS_HPP__
+
+#include "Eigen/Dense"
+
+class Distance {
+public:
+    /*
+     * d = ((x1-x2)^2 + (y1-y2)^2 + ...)^(1/2)
+     * */
+    static double euclideanDistance(const Eigen::VectorXd &a,
+                const Eigen::VectorXd &b) {
+        Eigen::VectorXd x = a - b;
+        Eigen::VectorXd c;
+        for (int i = 0; i < x.size(); ++i) {
+            c << power(x[i], 2);
+        }
+
+        return c.sum();
+    }
+
+};
+
+#endif
